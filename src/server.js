0677e7af395666f660d2fecef19dc698b0229457
Nicholas <nssouto@hotmail.com>
2023-06-01 08:49:19 -0300

    First Commit

---
+++ src/server.js
@@ -0,0 +1,38 @@
+require("express-async-errors");
+
+const migrationsRun = require("./database/sqlite/migrations");
+
+const cors = require("cors");
+const AppError = require("./utils/appError");
+const uploadConfig = require("./configs/upload");
+const express = require("express");
+const routes = require("./routes");
+
+migrationsRun();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
+
+app.use(routes);
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+
+  console.error(error);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+});
+
+const PORT = 3333;
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
